a58643b0f1223378a6802f6acc178e34b76dbb1a
David Yip <yipdw@northwestern.edu>
2009-04-13 02:57:53 -0500

    Eliminated unnecesary method.

--- lib/dagnabit/activation.rb
+++ lib/dagnabit/activation.rb
@@ -3,16 +3,12 @@ module Dagnabit
     def acts_as_dag_link(options = {})
       initialize_acts_as_dag_link
       configure_acts_as_dag_link(options)
-      add_edge_class_methods
     end
     
     private
 
     def initialize_acts_as_dag_link
       extend(Dagnabit::Link::Configuration)
-    end
-
-    def add_edge_class_methods
       extend(Dagnabit::Link::ClassMethods)
     end
   end
