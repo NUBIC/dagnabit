c2473980b2915c59f80df606a54de52548bb8580
David Yip <yipdw@northwestern.edu>
2009-04-27 14:27:14 -0500

    Documented validations.

--- lib/dagnabit/link/validations.rb
+++ lib/dagnabit/link/validations.rb
@@ -1,5 +1,19 @@
 module Dagnabit
   module Link
+    #
+    # Basic validations on link models.
+    #
+    # This module only installs ancestor and descendant presence validations;
+    # the only basic requirement for a link is that it have a start point and an
+    # end point.  We validate the presence of the +ancestor+ and +descendant+,
+    # instead of +ancestor_id+ and +descendant_id+ in order to permit scenarios
+    # like this:
+    #
+    #   n1 = Node.new
+    #   n2 = Node.new
+    #   l = Link.new(:ancestor => n1, :descendant => n2)
+    #   l.save  # will save l, n1, and n2
+    #
     module Validations
       def self.extended(base)
         base.send(:validates_presence_of, :ancestor)
