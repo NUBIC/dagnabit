00e10823c26698327b43f1268836dad910da614f
David Yip <yipdw@northwestern.edu>
2009-07-30 10:54:39 -0500

    Accommodate the Ruby SQLite3 driver in its two dominant distribution forms (gem, on Ruby load path).

--- test/connections/native_sqlite3/connection.rb
+++ test/connections/native_sqlite3/connection.rb
@@ -2,6 +2,16 @@ print "Using native SQLite3\n"
 
 require 'logger'
 
+# The SQLite3 Ruby driver can be present as either a gem or directly present in
+# the Ruby library load path.  This load method addresses these two situations.
+begin
+  require 'sqlite3'
+rescue LoadError
+  require 'rubygems'
+  gem 'sqlite3-ruby'
+  require 'sqlite3'
+end
+
 ActiveRecord::Base.logger = Logger.new("debug.log")
 
 class SqliteError < StandardError
