96671fb66db05af8ab6f229089492d1e09bc138b
David Yip <yipdw@member.fsf.org>
2010-11-28 22:01:57 -0700

    Expanded the description of Dagnabit::Graph.

--- lib/dagnabit/graph.rb
+++ lib/dagnabit/graph.rb
@@ -2,7 +2,27 @@ require File.join(File.dirname(__FILE__), %w(.. dagnabit))
 
 module Dagnabit
   ##
-  # A set comprising a collection of vertices and edges.
+  # A graph is a pair (V, E) where V is a set of vertices and E is a set of
+  # nodes.  This class (loosely) implements that definition and adds some
+  # useful utility methods.
+  #
+  # This class is a loose implementation of that definition for a number of
+  # reasons.  Here are a couple:
+  #
+  # * It represents the vertex and edge sets using data structures more akin to
+  #   bags than sets.
+  # * It imposes an additional restriction on the form of vertices and edges;
+  #   namely, that they must be ActiveRecord::Base subclasses that extend
+  #   {Vertex::Connectivity}.
+  #
+  # Despite these flaws, it is hoped that {Graph} is still useful for
+  # performing queries on graphs built with dagnabit.
+  #
+  # {Graph} is not compatible with RGL's Graph concept.  This is because
+  # {Graph} defines {#vertices} and {#edges} as accessors, whereas RGL provides
+  # implementations of {#vertices} and {#edges} in terms of methods called
+  # `each_vertex` and `each_edge`, respectively.  Nevertheless, RGL compatibility would be
+  # nice to have, so it is a (currently low priority) to-do item.
   #
   # Graphs may be constructed from a set of source nodes with {.from_roots}.
   class Graph
