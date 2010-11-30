0f976b00495fc3eb7e9b7aa6588972314f31c58b
David Yip <yipdw@member.fsf.org>
2010-11-29 20:52:48 -0600

    Added option to associate an edge model with a vertex model.

--- lib/dagnabit/vertex/settings.rb
+++ lib/dagnabit/vertex/settings.rb
@@ -21,6 +21,33 @@ module Dagnabit::Vertex
     end
 
     ##
+    # The edge model associated with a vertex model.  Defaults to nil.
+    #
+    # Connectivity queries do not use this model; however, some Vertex modules,
+    # such as {Bonding}, do.  If you do not intend to use any of those modules,
+    # it is safe to leave this as nil.
+    #
+    # @return [Class] the edge model
+    def edge_model
+      @edge_model
+    end
+
+    ##
+    # Sets the edge model.
+    #
+    # Setting this will also set the edge table to the return value of
+    #
+    #     model.table_name
+    # .
+    #
+    # @param [#table_name] model an ActiveRecord::Base subclass
+    def set_edge_model(model)
+      @edge_model = model
+
+      set_edge_table(model.table_name) if model
+    end
+
+    ##
     # This callback ensures that whenever a class that extends Settings is
     # inherited, the settings for the superclass are passed to the subclass.
     #
@@ -31,6 +58,7 @@ module Dagnabit::Vertex
     def inherited(subclass)
       super
       subclass.set_edge_table(edge_table)
+      subclass.set_edge_model(edge_model)
     end
   end
 end
--- spec/dagnabit/vertex/settings_spec.rb
+++ spec/dagnabit/vertex/settings_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
+require 'models/edge'
 require 'models/vertex'
 
 module Dagnabit::Vertex
   describe Settings do
     let(:model) { Class.new(Vertex) }
+    let(:edge) { Edge[Vertex] }
 
     before do
       model.extend(Settings)
@@ -21,6 +23,46 @@ module Dagnabit::Vertex
         model.set_edge_table 'other_edges'
 
         model.edge_table.should == 'other_edges'
+      end
+    end
+
+    describe '#set_edge_model' do
+      it 'sets the edge model' do
+        model.set_edge_model edge
+
+        model.edge_model.should == edge
+      end
+
+      it 'sets the edge table name' do
+        model.set_edge_table ''
+        model.set_edge_model edge
+
+        model.edge_table.should == edge.table_name
+      end
+
+      it 'does not set the edge table name if model is nil' do
+        model.set_edge_table 'my_edges'
+        model.set_edge_model nil
+
+        model.edge_table.should == 'my_edges'
+      end
+    end
+
+    describe '#inherited' do
+      it "copies the superclass' edge table to the subclass" do
+        model.set_edge_table 'my_edges'
+
+        subclass = Class.new(model)
+
+        subclass.edge_table.should == model.edge_table
+      end
+
+      it "copies the superclass' edge model to the subclass" do
+        model.set_edge_model edge
+
+        subclass = Class.new(model)
+
+        subclass.edge_model.should == model.edge_model
       end
     end
   end
