8934e39c51c74ee791b52a2628a50b0200eda252
David Yip <yipdw@northwestern.edu>
2010-12-01 16:57:12 -0600

    I really should follow my recommendations for schema design.

--- spec/schema.rb
+++ spec/schema.rb
@@ -1,21 +1,28 @@
 require 'active_record'
+require 'dagnabit'
 
 ActiveRecord::Schema.define do
-  create_table :edges, :force => true do |t|
-    t.integer :parent_id
-    t.integer :child_id
+  extend Dagnabit::Migration
+
+  execute 'DROP LANGUAGE IF EXISTS plpgsql CASCADE'
+  execute 'CREATE TRUSTED LANGUAGE plpgsql'
+
+  [:edges, :other_edges].each do |table|
+    create_table table, :force => true do |t|
+      t.references :parent, :null => false
+      t.references :child,  :null => false
+    end
+
+    add_index table, [:parent_id, :child_id], :unique => true
+
+    create_cycle_check_trigger table
   end
 
   create_table :vertices, :force => true do |t|
-    t.integer :ordinal
+    t.integer :datum
   end
 
   create_table :other_vertices, :force => true do |t|
-    t.integer :ordinal
-  end
-
-  create_table :other_edges, :force => true do |t|
-    t.integer :parent_id
-    t.integer :child_id
+    t.integer :datum
   end
 end
