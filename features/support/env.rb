fa73e1caabd8b2ada423ee4dc4aed180c6b9d17f
David Yip <yipdw@northwestern.edu>
2010-12-01 16:58:46 -0600

    more boilerplate

---
+++ features/support/env.rb
@@ -0,0 +1,20 @@
+database = ENV['DATABASE'] || 'postgresql'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), %w(.. .. lib)))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), %w(.. .. spec)))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), %w(.. .. spec connections), database))
+
+require 'connection'
+require 'schema'
+
+require 'dagnabit'
+require 'models/edge'
+require 'models/vertex'
+
+Before do
+  c = ActiveRecord::Base.connection
+
+  c.tables.each do |t|
+    c.execute("TRUNCATE TABLE #{c.quote_table_name(t)} CASCADE")
+  end
+end
