19c49d8bbd10d02294566b46c08f66ad40d0b8a0
David Yip <yipdw@member.fsf.org>
2010-12-02 09:14:45 -0600

    Miscellaneous fixes to Dagnabit::Graph's documentation.

--- lib/dagnabit/graph.rb
+++ lib/dagnabit/graph.rb
@@ -2,18 +2,13 @@ require File.join(File.dirname(__FILE__), %w(.. dagnabit))
 
 module Dagnabit
   ##
-  # A graph is a pair (V, E) where V is a set of vertices and E is a set of
-  # nodes.  This class (loosely) implements that definition and adds some
-  # useful utility methods.
-  #
-  # This class is a loose implementation of that definition for a number of
-  # reasons.  Here are a couple:
+  # This class is a representation of a directed graph.  It deviates from the
+  # definition of a directed graph in a few ways; here's a couple:
   #
   # * It represents the vertex and edge sets using data structures more akin to
   #   bags than sets.
-  # * It imposes an additional restriction on the form of vertices and edges;
-  #   namely, that they must be ActiveRecord::Base subclasses that extend
-  #   {Vertex::Connectivity}.
+  # * It imposes restrictions on the form of vertices and edges; namely, they
+  #   must be ActiveRecord::Base subclasses that extend {Vertex::Connectivity}.
   #
   # Despite these flaws, it is hoped that {Graph} is still useful for
   # performing queries on graphs built with dagnabit.
@@ -21,8 +16,8 @@ module Dagnabit
   # {Graph} is not compatible with RGL's Graph concept.  This is because
   # {Graph} defines {#vertices} and {#edges} as accessors, whereas RGL provides
   # implementations of {#vertices} and {#edges} in terms of methods called
-  # `each_vertex` and `each_edge`, respectively.  Nevertheless, RGL compatibility would be
-  # nice to have, so it is a (currently low priority) to-do item.
+  # `each_vertex` and `each_edge`, respectively.  Nevertheless, RGL
+  # compatibility would be nice to have, so it is a to-do item.
   #
   # Graphs may be constructed from a set of source nodes with {.from_vertices}.
   class Graph
