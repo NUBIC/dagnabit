a8d8c246fb3063614c17a3e48026cfcd213f10ea
David Yip <yipdw@northwestern.edu>
2011-02-10 16:44:44 -0600

    Documentation for be_set_equivalent.

--- spec/matchers/be_set_equivalent.rb
+++ spec/matchers/be_set_equivalent.rb
@@ -1,3 +1,8 @@
+##
+# Returns true if two finite sets are equivalent, false otherwise.
+#
+# This matcher expects its operands to respond to #to_a, which in turn should
+# return something enumerable.
 RSpec::Matchers.define(:be_set_equivalent_to) do |*expected|
   match do |actual|
     Set.new(actual.to_a).should == Set.new(expected.to_a)
