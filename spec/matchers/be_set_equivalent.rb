918b48c9eaf4f210fa30a6b288a314ccec9d019c
David Yip <yipdw@northwestern.edu>
2011-02-10 16:40:42 -0600

    Ensure that be_set_equivalent gets something enumerable.
    (Scopes aren't enumerable in ActiveRecord 3.)

--- spec/matchers/be_set_equivalent.rb
+++ spec/matchers/be_set_equivalent.rb
@@ -1,5 +1,5 @@
 RSpec::Matchers.define(:be_set_equivalent_to) do |*expected|
   match do |actual|
-    Set.new(actual).should == Set.new(expected)
+    Set.new(actual.to_a).should == Set.new(expected.to_a)
   end
 end
