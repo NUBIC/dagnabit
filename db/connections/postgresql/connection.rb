2e8d0e40644fe3d2780417883d8335cd59769b77
David Yip <yipdw@northwestern.edu>
2011-08-31 18:33:32 -0500

    Travis setup, try 1.

--- db/connections/postgresql/connection.rb
+++ db/connections/postgresql/connection.rb
@@ -5,14 +5,24 @@ puts 'Using PostgreSQL adapter'
 
 ActiveRecord::Base.logger = Logger.new('debug.log')
 
-ActiveRecord::Base.configurations = {
-  'ActiveRecord::Base' => {
-    :adapter => 'postgresql',
-    :database => 'dagnabit_test',
-    :username => 'dagnabit_test',
-    :password => 'dagnabit_test',
-    :host => 'localhost'
+if ENV['TRAVIS']
+  ActiveRecord::Base.configurations = {
+    'ActiveRecord::Base' => {
+      :adapter => 'postgresql',
+      :database => 'dagnabit_test',
+      :username => 'postgres'
+    }
   }
-}
+else
+  ActiveRecord::Base.configurations = {
+    'ActiveRecord::Base' => {
+      :adapter => 'postgresql',
+      :database => 'dagnabit_test',
+      :username => 'dagnabit_test',
+      :password => 'dagnabit_test',
+      :host => 'localhost'
+    }
+  }
+end
 
 ActiveRecord::Base.establish_connection('ActiveRecord::Base')
