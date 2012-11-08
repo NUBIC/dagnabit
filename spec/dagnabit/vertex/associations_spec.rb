ec290106bf98ef4d7a2b8d48482e8b812aeaeb95
David Yip <yipdw@northwestern.edu>
2012-11-08 04:21:15 +0000

    Demonstrate mutation of #parents and #children associations.

--- spec/dagnabit/vertex/associations_spec.rb
+++ spec/dagnabit/vertex/associations_spec.rb
@@ -7,9 +7,13 @@ module Dagnabit::Vertex
   describe Associations do
     [:v1, :v2].each { |v| let(v) { ::Vertex.create } }
 
-    let!(:e) { ::Edge.create(:parent_id => v1.id, :child_id => v2.id) }
+    shared_context 'edge' do
+      let!(:e) { ::Edge.create(:parent_id => v1.id, :child_id => v2.id) }
+    end
 
     describe '#in_edges' do
+      include_context 'edge'
+
       it 'returns the edges whose tail is the receiver vertex' do
         v2.in_edges.should == [e]
       end
@@ -22,6 +26,8 @@ module Dagnabit::Vertex
     end
 
     describe '#out_edges' do
+      include_context 'edge'
+
       it 'returns the edges whose head is the receiver vertex' do
         v1.out_edges.should == [e]
       end
@@ -30,6 +36,22 @@ module Dagnabit::Vertex
         v1.destroy
 
         Edge.count.should == 0
+      end
+    end
+
+    describe '#parents' do
+      it 'accepts new vertices' do
+        v1.parents << v2
+
+        Vertex.find(v1).parents.should == [v2]
+      end
+    end
+
+    describe '#children' do
+      it 'accepts new vertices' do
+        v1.children << v2
+
+        Vertex.find(v1).children.should == [v2]
       end
     end
   end
